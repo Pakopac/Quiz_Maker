79b2a1eb12a13d2429849a64b9f7976739714c86
SUP'Internet 09 <supinternet09@MacBook-Air-de-SUPInternet-9.local>
2019-11-25 17:01:09 +0100

    start login

--- Client/Client/ViewController.swift
+++ Client/Client/ViewController.swift
@@ -31,6 +31,9 @@ extension UIView {
 
 class ViewController: UIViewController {
     
+    let email: UITextField = UITextField()
+    let password: UITextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,14 +43,12 @@ class ViewController: UIViewController {
         helloView.text = "Welcome to Quiz Maker"
         helloView.textAlignment = .center
         
-        let username = UITextField()
-        username.backgroundColor = UIColor.white
-        username.placeholder = "username"
-        username.layer.cornerRadius = 10
-        username.setLeftPaddingPoints(10)
-        username.returnKeyType = UIReturnKeyType.next
+        email.backgroundColor = UIColor.white
+        email.placeholder = "email"
+        email.layer.cornerRadius = 10
+        email.setLeftPaddingPoints(10)
+        email.returnKeyType = UIReturnKeyType.next
         
-        let password = UITextField()
         password.backgroundColor = UIColor.white
         password.placeholder = "password"
         password.layer.cornerRadius = 10
@@ -61,19 +62,66 @@ class ViewController: UIViewController {
         button.backgroundColor = UIColor.black
         
         button.addTarget(self, action: #selector(redirectToList), for: .touchUpInside)
-    
+        
         
         self.view.backgroundColor = UIColor.gray
         self.view.grid(child:nView, x: 0, y: 2, height: 4, width: 12)
         nView.grid(child: helloView, x: 3, y: 2, height: 4, width: 6)
-        self.view.grid(child:username, x: 2, y: 5, height: 0.5, width: 8)
+        self.view.grid(child:email, x: 2, y: 5, height: 0.5, width: 8)
         self.view.grid(child:password, x: 2, y: 6, height: 0.5, width: 8)
         self.view.grid(child:button, x: 4, y: 7, height: 0.5, width: 4)
         
     }
     
-    @objc func redirectToList(username: UITextField) {
-        print(self)
+    @objc func redirectToList() {
+        print("email",email)
+        let json: [String: Any] = [
+            "email": self.email.text!,
+            "password": self.password.text!
+        ]
+        
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        let url = URL(string: "http://edu2.shareyourtime.fr/api/auth")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            do {
+                let responseJSON = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:Any]
+                let data = responseJSON["data"] as! [String:Any]
+                let token = data["token"] as! String
+                
+                let urlToken = URL(string: "http://edu2.shareyourtime.fr/api/secret")!
+                var requestToken = URLRequest(url: urlToken)
+                requestToken.httpMethod = "POST"
+                let authValue: String? = "Bearer \(token)"
+                requestToken.setValue("Authorization", forHTTPHeaderField: authValue!)
+                
+                let taskToken = URLSession.shared.dataTask(with: requestToken) { data, response, error in
+                    do {
+                        let responseToken = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:Any]
+                      
+                      print("ok2")
+                      print(responseToken)
+                    }
+                    catch let error as NSError{
+                        print("ok")
+                        print(error)
+                    }
+                }
+                
+                taskToken.resume()
+                print(token)
+            }
+            catch let error as NSError{
+                print(error)
+            }
+        }
+        
+        task.resume()
         self.performSegue(withIdentifier: "ShowSecondVIew", sender: nil)
     
     }
