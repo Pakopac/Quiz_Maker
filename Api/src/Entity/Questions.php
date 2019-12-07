ec44da34f30729b14c0daede9cf8928110d18921
kainister <kevinb@MacBook-Pro-de-Kevin.local>
2019-12-07 16:59:34 +0100

    Added score table

--- Api/src/Entity/Questions.php
+++ Api/src/Entity/Questions.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
+  @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\QuestionsRepository")
  */
 class Questions
---
+++ Api/src/Entity/Score.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ScoreRepository")
+ */
+class Score
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quizId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuizId(): ?int
+    {
+        return $this->quizId;
+    }
+
+    public function setQuizId(int $quizId): self
+    {
+        $this->quizId = $quizId;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+}
